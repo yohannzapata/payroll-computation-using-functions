4431a576d364c2babf3dac6bc00bc9943010a7d5
Yohann Zapata <yohannjoachim@gmail.com>
2025-11-08 01:33:50 +0800

    changes

--- Main.java
+++ Main.java
@@ -4,16 +4,51 @@ public class Main
 {
     public static Scanner sc = new Scanner(System.in);
 
+    public static String last_name;
+    public static String first_name;
+    public static String middle_name;
+    public static String department;
+    public static String position;
+    public static int rate_per_hour;
+    public static int num_of_hours_worked;
+
     public static void main(String[] args)
     {
-        String employee = sc.nextLine();
-        
-        
+        employee_info();
+    }
 
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+    public static void employee_info() {
+        System.out.println("Enter Employee Information:");
+        System.out.print("Last Name               : ");
+        last_name = sc.nextLine();
+
+        System.out.print("First Name              : ");
+        first_name = sc.nextLine();
+
+        System.out.print("Middle Name             : ");
+        middle_name = sc.nextLine();
+
+        System.out.print("Department              : ");
+        department = sc.nextLine();
+
+        System.out.print("Position                : ");
+        position = sc.nextLine();
+
+        System.out.print("Rate Per Hour           : ");
+        rate_per_hour = sc.nextInt();
+
+        System.out.print("Number of Hours Worked  : ");
+        num_of_hours_worked = sc.nextInt();
+    }
+
+    public static void summary_pay_slip() {
+        /* 
+            The program should display a summary or pay slip of an employee showing:
+                a. Employee Information
+                b. Details of the Salary Computation
+                c. Gross Pay
+                d. Net Pay
+        */
+
     }
 }
